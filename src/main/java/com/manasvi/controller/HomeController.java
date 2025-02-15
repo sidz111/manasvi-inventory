fa3942876ab869186e1ed39afc9a1b2363aee476
sidz111 <sssurwade2212@gmail.com>
2025-02-15 11:30:30 +0530

    🔔🚀💻 Added notification module! Optimized performance, fixed bugs, and enhanced security 🔐✨🔧🛡️

--- src/main/java/com/manasvi/controller/HomeController.java
+++ src/main/java/com/manasvi/controller/HomeController.java
@@ -1,36 +1,59 @@
-	package com.manasvi.controller;
-	
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.ui.Model;
-	import org.springframework.web.bind.annotation.GetMapping;
+package com.manasvi.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.manasvi.entity.Tea;
 import com.manasvi.service.StockHistoryService;
 import com.manasvi.service.StockItemService;
-	import com.manasvi.service.TeaService;
-	import com.manasvi.service.WaterJarService;
-	
-	@Controller
-	public class HomeController {
-		
-		@Autowired
-		StockItemService stockItemService;
-		
-		@Autowired
-		TeaService teaService;
-		
-		@Autowired
-		WaterJarService waterJarService;
-		
-		@Autowired
-		StockHistoryService stockHistoryService;
-		
-	    @GetMapping("/")
-	    public String home(Model model) {
-			model.addAttribute("totalStocks", stockItemService.getAllStockItems());
-	    	model.addAttribute("totalTeas", teaService.getAllTeaRecords().size());
-	    	model.addAttribute("totalWaterJars", waterJarService.getAllWaterJars().size());
-	    	model.addAttribute("allStockHistories", stockHistoryService.getAllStockHistories());
-	        return "index";
-	    }
+import com.manasvi.service.TeaService;
+import com.manasvi.service.WaterJarService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	StockItemService stockItemService;
+
+	@Autowired
+	TeaService teaService;
+
+	@Autowired
+	WaterJarService waterJarService;
+
+	@Autowired
+	StockHistoryService stockHistoryService;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("totalStocks", stockItemService.getAllStockItems());
+		model.addAttribute("totalTeas", teaService.getAllTeaRecords().size());
+		model.addAttribute("totalWaterJars", waterJarService.getAllWaterJars().size());
+		model.addAttribute("allStockHistories", stockHistoryService.getAllStockHistories());
+		List<Tea> allTeas = teaService.getAllTeaRecords();
+
+		int morningTeasTotal = allTeas.stream().filter(
+				tea -> "Morning".equalsIgnoreCase(tea.getShip()) && extractDate(tea.getDate()).equals(LocalDate.now()))
+				.mapToInt(Tea::getTotalTeaCups).sum();
+
+		int eveningTeasTotal = allTeas.stream().filter(
+				tea -> "Evening".equalsIgnoreCase(tea.getShip()) && extractDate(tea.getDate()).equals(LocalDate.now()))
+				.mapToInt(Tea::getTotalTeaCups).sum();
+
+		model.addAttribute("morningTeasTotal", morningTeasTotal);
+		model.addAttribute("eveningTeasTotal", eveningTeasTotal);
+		return "index";
 	}
+	private LocalDate extractDate(String dateTimeString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+        return dateTime.toLocalDate();
+    }
+}
