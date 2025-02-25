4a155f3d455048416449070d13a69bf2a115ca19
sidz111 <sssurwade2212@gmail.com>
2025-02-25 11:07:53 +0530

    Update UI of Dashboard

--- src/main/java/com/manasvi/entity/StockItem.java
+++ src/main/java/com/manasvi/entity/StockItem.java
@@ -11,65 +11,65 @@ import jakarta.persistence.OneToMany;
 
 @Entity
 public class StockItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private Integer totalStocks;
+	private Integer totalStocks;
 
-    private String addedDate;
-    private String updateDate;
+	private String addedDate;
+	private String updateDate;
 
-    @OneToMany(mappedBy = "stockItem", cascade = CascadeType.ALL)
-    private List<StockHistory> historyRecords;
+	@OneToMany(mappedBy = "stockItem", cascade = CascadeType.ALL)
+	private List<StockHistory> historyRecords;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Integer getTotalStocks() {
-        return totalStocks;
-    }
+	public Integer getTotalStocks() {
+		return totalStocks;
+	}
 
-    public void setTotalStocks(Integer totalStocks) {
-        this.totalStocks = totalStocks;
-    }
+	public void setTotalStocks(Integer totalStocks) {
+		this.totalStocks = totalStocks;
+	}
 
-    public String getAddedDate() {
-        return addedDate;
-    }
+	public String getAddedDate() {
+		return addedDate;
+	}
 
-    public void setAddedDate(String addedDate) {
-        this.addedDate = addedDate;
-    }
+	public void setAddedDate(String addedDate) {
+		this.addedDate = addedDate;
+	}
 
-    public String getUpdateDate() {
-        return updateDate;
-    }
+	public String getUpdateDate() {
+		return updateDate;
+	}
 
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
-    }
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
 
-    public List<StockHistory> getHistoryRecords() {
-        return historyRecords;
-    }
+	public List<StockHistory> getHistoryRecords() {
+		return historyRecords;
+	}
 
-    public void setHistoryRecords(List<StockHistory> historyRecords) {
-        this.historyRecords = historyRecords;
-    }
+	public void setHistoryRecords(List<StockHistory> historyRecords) {
+		this.historyRecords = historyRecords;
+	}
 }
