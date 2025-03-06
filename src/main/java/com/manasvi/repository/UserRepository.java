f6f54325d4883de7091105d139bbca23298890fe
sidz111 <sssurwade2212@gmail.com>
2025-03-06 17:07:51 +0530

    started Spring security

---
+++ src/main/java/com/manasvi/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.manasvi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manasvi.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByEmail(String email);
+}
---
+++ src/main/java/com/manasvi/service/UserService.java
@@ -0,0 +1,20 @@
+package com.manasvi.service;
+
+import java.util.List;
+
+import com.manasvi.entity.User;
+
+public interface UserService {
+
+	User addUser(User user);
+	
+	User updateUser(User user);
+	
+	User getUserById(Integer id);
+	
+	User getUserByEmail(String email);
+	
+	List<User> getAllUsers();
+	
+	void deleteUserById(Integer id);
+}
