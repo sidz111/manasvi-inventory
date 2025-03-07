72b1ca1587d2b64e61de3373dcac5114d06b156a
sidz111 <sssurwade2212@gmail.com>
2025-03-07 17:44:35 +0530

    Nothing

--- src/main/java/com/manasvi/repository/UserRepository.java
+++ src/main/java/com/manasvi/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.manasvi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.manasvi.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByName(String name);
-    
+    List<User> findByRole(String role);
     User findByEmail(String email);
 }
